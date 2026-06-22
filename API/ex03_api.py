3bcc0d6d293e507f9415afdcda338c23d35699fb
Lucas Lemke <lucas.diogenes09@gmail.com>
2026-06-22 16:40:07 -0300

    api problema

--- API/ex03_api.py
+++ API/ex03_api.py
@@ -1,20 +1,17 @@
 from flask import Flask
-from datetime import date  # Importando a dica do professor
-
+from datetime import date
 app = Flask(__name__)
 
-# Rota 1: Saudação
 @app.route('/saudacao')
 def saudacao():
-    return "Seja muito bem-vindo à nossa API Flask!"
+    return "bem vindo"
 
-# Rota 2: Data de hoje
 @app.route('/data')
 def data_atual():
     hoje = date.today()
-    # Formatando a data para o padrão brasileiro (DD/MM/AAAA)
+
     data_formatada = hoje.strftime('%d/%m/%Y')
-    return f"A data de hoje é: {data_formatada}"
+    return f"a data de hoje é: {data_formatada}"
 
 if __name__ == '__main__':
     app.run(debug=True)
